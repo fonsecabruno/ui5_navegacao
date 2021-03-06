525f2e73662b502851e9136c187c8fe9980e29fd
fonsecabruno <fonsecabrunocosta@gmail.com>
2021-03-05 23:04:01 -0300

    Categorie Save in Odata

--- webapp/controller/NovaCategoria.controller.js
+++ webapp/controller/NovaCategoria.controller.js
@@ -1,22 +1,70 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast",
+	"sap/m/MessageBox"
+
 ],
 	/**
 	 * @param {typeof sap.ui.core.mvc.Controller} Controller
 	 */
-	function (Controller) {
+	function (Controller, MessageBox, MessageToast) {
 		"use strict";
-
 		return Controller.extend("ovly.ui5navegacao.controller.NovaCategoria", {
+			// oRouter: null,
 			onInit: function () {
 				this.oRouter = this.getOwnerComponent().getRouter();
+				this.oRouter
+					.getRoute("novaCategoria")
+					.attachPatternMatched(this.onPatternMached, this);
 			},
-			onNavBack: function (){
+			onPatternMached: function (oEvent) {
+				// @type sap.ui.model.odata.v2.ODataModel
+				var oModel = this.getView().getModel();
+				var oPromiseMetadataLoaded = this.getView().getModel().metadataLoaded();
+				oPromiseMetadataLoaded.then(function () {
+					this._oContext = oModel.createEntry("/Categories", {});
+					this.getView().setBindingContext(this._oContext);
+				}.bind(this));
+			},
+			onNavBack: function () {
 				this.oRouter.navTo("padrao");
+			},
+			onSave: function () {
 
-			},
-			onCancel: function () {
-				this.oRouter.navTo("padrao");
+
+				function onSuccess(oNovaCategoria) {
+
+					MessageToast.show("Coleção cadastrada com sucesso", {
+
+						closeOnBrowserNavigation: false
+
+					});
+
+					this.oRouter.navTo("detalhe", {
+
+						id: this._oContext.getProperty("ID")
+
+					});
+
+				}
+
+
+
+				function onError(oError) {
+
+					MessageBox.error("Erro na criação da categoria");
+
+				}
+
+
+
+				this.getView().getModel().submitChanges({
+
+					success: onSuccess.bind(this),
+
+					error: onError.bind(this)
+
+				});
 
 			}
 		});
--- webapp/controller/Produtos.controller.js
+++ webapp/controller/Produtos.controller.js
@@ -2,17 +2,30 @@ sap.ui.define([
 	"ovly/ui5navegacao/controller/BaseController",
 	"ovly/ui5navegacao/model/formatter"
 ],
-	function (BaseController, formatter ) {
+	function (BaseController, Formatter ) {
 		"use strict";
 
 		return BaseController.extend("ovly.ui5navegacao.controller.Produtos", {
+
+		   formatter : Formatter,
+
 			onInit: function() {
 				let oRoute = this.getRouter().getRoute("detalhe");
-				oRoute.attachPatternMatched(this.onPBO);
+				oRoute.attachPatternMatched(this.onPatternMatched, this);
 			},
 
-			onPBO: function() {
-				console.log("caiu");
+			onPatternMatched: function(oEvent) {
+				let oParameters = oEvent.getParameters();
+				let oArguments = oParameters.arguments;
+			
+				let sPath = this.getModel().createKey("Categories", {
+					ID: oArguments.id
+				});
+				// console.log(oArguments.id);
+				this.getView().bindElement({
+					path: "/" + sPath
+
+				});
 			},
 
 			onNavBack: function() {
